507b738b0a79a058693160e7b061e5ae59dfe94c
Filip Antala <filip@ahst.sk>
2022-12-31 12:09:00 +0100

    formatting + autocreate datadir

--- index.js
+++ index.js
@@ -1,62 +1,66 @@
-const Express = require('express');
-const fs = require('fs');
-const p = require('path');
-const { Server } = require('socket.io');
-const http = require('http');
-const { createTerminalInterface } = require('./terminal');
-const Counter = require('./counter');
+const Express = require("express");
+const fs = require("fs");
+const p = require("path");
+const { Server } = require("socket.io");
+const http = require("http");
+const { createTerminalInterface } = require("./terminal");
+const Counter = require("./counter");
 const app = Express();
 const server = http.createServer(app).listen(3015);
 const { instrument } = require("@socket.io/admin-ui");
 const io = new Server(server, {
-	cors: {
-		origin: ["https://admin.socket.io", "http://localhost:3000"],
-		credentials: true
-	}
+  cors: {
+    origin: ["https://admin.socket.io", "http://localhost:3000"],
+    credentials: true,
+  },
 });
 instrument(io, { auth: false });
 
-app.use(Express.static('./web/build'));
-
+app.use(Express.static("./web/build"));
 
 const interface = createTerminalInterface(io);
 
-const dataDir = fs.readdirSync(p.join(__dirname, 'data'), { withFileTypes: true }).filter((dir) => dir.isFile());
-dataDir.forEach(dataFile => {
-	if (!dataFile.name.endsWith('.json')) return;
-	const data = require(`./data/${dataFile.name}`);
-	const name = data?.name ?? dataFile.name.replace('.json', '');
+if (!fs.existsSync(p.join(__dirname, "data"))) {
+  fs.mkdirSync(p.join(__dirname, "data"));
+}
+const dataDir = fs
+  .readdirSync(p.join(__dirname, "data"), { withFileTypes: true })
+  .filter((dir) => dir.isFile());
+dataDir.forEach((dataFile) => {
+  if (!dataFile.name.endsWith(".json")) return;
+  const data = require(`./data/${dataFile.name}`);
+  const name = data?.name ?? dataFile.name.replace(".json", "");
 
-	const ctr = new Counter(name, io);
+  const ctr = new Counter(name, io);
 
-	console.log(`Created counter: ${ctr.name}`);
+  console.log(`Created counter: ${ctr.name}`);
 });
 
+io.on("connection", (socket) => {
+  socket.emit("HELLO", { counters: Counter.counters.keys() });
+  socket.on("counters", (ack) => {
+    ack({ counters: Array.from(Counter.counters.keys()) });
+  });
 
-io.on('connection', socket => {
-	socket.emit('HELLO', { counters: Counter.counters.keys() });
-	socket.on('counters', (ack) => { ack({ counters: Array.from(Counter.counters.keys()) }) });
+  socket.on("subscribe", (ctr, ack) => {
+    // check if exists
+    const counter = Counter.get(ctr);
+    if (!counter) ack(false);
 
-	socket.on('subscribe', (ctr, ack) => {
-		// check if exists
-		const counter = Counter.get(ctr);
-		if (!counter) ack(false);
+    socket.join(counter.name);
+    ack(true);
+  });
+  socket.on("unsubscribe", (ctr, ack) => {
+    socket.leave(ctr);
+    ack(true);
+  });
 
-		socket.join(counter.name);
-		ack(true);
-	});
-	socket.on('unsubscribe', (ctr, ack) => {
-		socket.leave(ctr);
-		ack(true);
-	});
+  socket.on("time", (ctr, ack) => {
+    const counter = Counter.get(ctr);
+    if (!counter) ack(false);
 
-	socket.on('time', (ctr, ack) => {
-		const counter = Counter.get(ctr);
-		if (!counter) ack(false);
-
-		ack(counter.metadata());
-	});
+    ack(counter.metadata());
+  });
 });
-
 
 console.log(`Ready!`);
