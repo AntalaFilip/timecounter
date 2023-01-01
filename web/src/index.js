95d910a8c2ce136e25f3ef33af29c62362788cfe
Filip Antala <filip@ahst.sk>
2023-01-01 10:08:08 +0100

    added mui/joy, material icons

--- web/src/index.js
+++ web/src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { CssVarsProvider } from "@mui/joy/styles";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <CssVarsProvider defaultMode="dark">
+      <App />
+    </CssVarsProvider>
   </React.StrictMode>
 );
-
