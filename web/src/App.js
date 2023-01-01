57adcd762bcb5494561d47a5edd3dbb24f55c485
Filip Antala <filip@ahst.sk>
2023-01-01 10:08:36 +0100

    nicened the frontend

--- web/src/App.js
+++ web/src/App.js
@@ -1,97 +1,223 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import io from 'socket.io-client';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import io from "socket.io-client";
+import { motion } from "framer-motion";
+import useInterval from "./useInterval";
+import { DateTime } from "luxon";
+import Typography from "@mui/joy/Typography";
+import Sheet from "@mui/joy/Sheet";
+import Button from "@mui/joy/Button";
+import IconButton from "@mui/joy/IconButton";
+import List from "@mui/joy/List";
+import ListItem from "@mui/joy/ListItem";
+import ListSubheader from "@mui/joy/ListSubheader";
+import ListItemButton from "@mui/joy/ListItemButton";
+import Grid from "@mui/joy/Grid";
 
-import { motion } from 'framer-motion';
-import useInterval from './useInterval';
-
-const socket = io();
+const query = Object.fromEntries(
+  new URLSearchParams(window.location.search).entries()
+);
+const uri = query.endpoint ?? undefined;
+const socket = io(uri);
 
 function App() {
-	const [state, setState] = useState();
-	const [refresh, setRefresh] = useState(0);
-	const [show, setShow] = useState();
-	const [currentTime, setTime] = useState('Loading...');
+  const [state, setState] = useState();
+  const [refresh, setRefresh] = useState(0);
+  const [show, setShow] = useState();
+  const [currentTime, setTime] = useState("Loading...");
+  const [currentDate, setDate] = useState();
+  const [config, setConfig] = useState();
 
-	useEffect(() => {
-		socket.emit('counters', (res) => {
-			setState(res);
-		});
-	}, [refresh]);
+  useEffect(() => {
+    socket.emit("counters", (res) => {
+      setState(res);
+    });
+    socket.emit("config", (res) => {
+      setConfig(res);
+    });
+  }, [refresh]);
 
-	useEffect(() => {
-		if (show) {
-			socket.emit('time', show, (res) => {
-				setState(res);
-			});
-			socket.emit('subscribe', show, res => {
+  useEffect(() => {
+    const emitRefresh = () => {
+      socket.emit("time", show, (res) => {
+        setState(res);
+      });
+    };
 
-			});
-			socket.on('stateChange', (ctr, meta) => {
-				if (ctr !== show) return;
-				setState(meta);
-			});
-		}
-	}, [show]);
+    if (show) {
+      emitRefresh();
+      socket.emit("subscribe", show, (res) => {});
+      socket.on("stateChange", (ctr, meta) => {
+        if (ctr !== show) return;
+        setState(meta);
+      });
+      socket.io.on("reconnect", () => emitRefresh());
+    }
+  }, [show]);
 
-	const variants = {
-        hidden: { 
-        }, 
-        visible: { 
-            opacity: 1,
-            transition: { 
-                delayChildren: 1,
-                staggerChildren: 0.5
+  const changeShow = (ctr) => {
+    if (show) socket.emit("unsubscribe", show, (res) => {});
+    if (ctr) {
+      socket.emit("subscribe", ctr, (res) => {});
+    }
+    setShow(ctr);
+  };
+
+  const variants = {
+    hidden: {},
+    visible: {
+      opacity: 1,
+      transition: {
+        delayChildren: 1,
+        staggerChildren: 0.5,
+      },
+    },
+  };
+
+  const children = {
+    hidden: { opacity: 0, y: 20 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: { duration: 0.75, ease: "easeInOut" },
+    },
+  };
+
+  useInterval(() => {
+    if (!show || !state?.name) return;
+
+    const realDiff = Math.abs(Date.now() - state.elapsed);
+    /** how many TRUE ms since last data update */
+    const trueDiff = realDiff * state.speedModifier;
+    /** how many TRUE ms passed from start */
+    const trueMs = state.running
+      ? state.trueElapsed + trueDiff
+      : state.trueElapsed;
+    /** Current TRUE date, according to startPoint */
+    const trueDate = DateTime.fromMillis(trueMs + state.startPoint);
+    const timeFmt = query["format"] ?? `HH:mm:ss`;
+    const dateFmt = query["dateFormat"] ?? "";
+
+    setTime(trueDate.toUTC().toFormat(timeFmt));
+    setDate(trueDate.toUTC().toFormat(dateFmt));
+  }, 50);
+
+  const sopts = socket.io.opts;
+  console.log(state);
+
+  if (!show) {
+    return (
+      <Grid sx={{ flexGrow: 1 }} columns={12} container>
+        <Grid xs={4} />
+        <Grid
+          xs={4}
+          sx={{
+            justifyContent: "center",
+            alignItems: "center",
+            display: "flex",
+          }}
+        >
+          <Sheet sx={{ borderRadius: "sm", p: "5rem" }}>
+            <Typography level="h1" variant="solid">
+              TimeCounter
+            </Typography>
+            <Typography gutterBottom sx={{ mt: 1 }} level="h5">
+              {config?.name ?? "..."}
+            </Typography>
+            <Typography>{config?.motd ?? ""}</Typography>
+            <br />
+            <List
+              variant="outlined"
+              sx={{
+                bgcolor: "background.body",
+                borderRadius: "sm",
+                boxShadow: "sm",
+              }}
+            >
+              <ListSubheader
+                sx={{ display: "flex", justifyContent: "space-between" }}
+              >
+                Available counters
+                <IconButton
+                  sx={{ ml: 2 }}
+                  onClick={() => setRefresh(refresh + 1)}
+                >
+                  <span class="material-symbols-outlined">refresh</span>
+                </IconButton>
+              </ListSubheader>
+              {Array.isArray(state?.counters) ? (
+                state.counters.map((ctr) => (
+                  <ListItem variant="plain" key={ctr}>
+                    <ListItemButton onClick={() => changeShow(ctr)}>
+                      {ctr}
+                    </ListItemButton>
+                  </ListItem>
+                ))
+              ) : (
+                <ListItem>No counters currently available</ListItem>
+              )}
+            </List>
+            <Typography sx={{ mt: 2 }} level="body3">
+              <b>{socket.connected ? "Connected to" : "Disconnected from"}:</b>{" "}
+              {sopts.hostname}:{sopts.port}
+              {sopts.path}
+            </Typography>
+            <Typography level="body4">
+              Made with ❤️ by{" "}
+              <a rel="noreferrer" target="_blank" href="https://filip.ahst.sk">
+                Filip Antala
+              </a>{" "}
+              /{" "}
+              <a rel="noreferrer" target="_blank" href="https://ahst.sk">
+                AntalaHosted
+              </a>
+            </Typography>
+          </Sheet>
+        </Grid>
+        <Grid xs={4} />
+      </Grid>
+    );
+  } else {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <p style={{ marginLeft: 5 }}>{state.name}</p>
+          <Button
+            variant="plain"
+            startDecorator={
+              <span class="material-symbols-outlined">arrow_back</span>
             }
-        }
-    };
-
-	const children = {
-        hidden: { opacity: 0, y: 20 }, 
-        visible: { opacity: 1, y: 0, transition: { duration: 0.75, ease: 'easeInOut', } }
-    };
-
-	
-	useInterval(() => {
-		if (!show || !state?.name) return;
-
-		const realDiff = Math.abs(Date.now() - state.elapsed);
-		/** how many TRUE ms since last data update */
-		const trueDiff = realDiff * state.speedModifier;
-		/** how many TRUE ms passed from start */
-		const trueMs = state.running ? state.trueElapsed + trueDiff : state.trueElapsed;
-		/** Current TRUE date, according to startPoint */
-		const trueDate = new Date(trueMs + state.startPoint);
-
-		setTime(trueDate.toUTCString());
-	}, 50)	
-
-	const sopts = socket.io.opts;
-	console.log(state);
-
-	if (!show) {
-		return (
-			<div className="App">
-				<div className="App-header">
-					<h1>TimeCounter</h1>
-					<p>Available counters: </p><button onClick={() => setRefresh(refresh + 1)}>Refresh counters</button>
-					{Array.isArray(state?.counters) ? state.counters.map(ctr => (<button key={ctr} onClick={() => setShow(ctr)}>{ctr}</button>)) : <p>No counters currently available</p>}
-					<small>{socket.connected ? <p>Connected to: <code>{sopts.hostname}:{sopts.port}{sopts.path}</code></p> : `Disconnected`}</small>
-				</div>
-			</div>
-		);
-	}
-	else {
-		return (
-			<div className="App">
-				<div className="App-header">
-					<motion.div variants={variants} initial="hidden" animate="visible">
-						<motion.h4 variants={children}>{currentTime}</motion.h4>
-					</motion.div>
-				</div>
-			</div>
-		)
-	}
+            onClick={() => changeShow()}
+          >
+            Back
+          </Button>
+          <motion.div
+            className="App-content"
+            variants={variants}
+            initial="hidden"
+            animate="visible"
+          >
+            <motion.h2 variants={children}>{currentTime}</motion.h2>
+            <motion.h3 variants={children}>{currentDate}</motion.h3>
+            <motion.h5
+              style={{ color: state.running ? "lime" : "orangered" }}
+              variants={children}
+            >
+              Time is currently {state.running ? `running` : `paused`} at{" "}
+              {state.speedModifier}x speed
+            </motion.h5>
+            <motion.h6
+              style={{ color: socket.connected ? "white" : "red" }}
+              variants={children}
+            >
+              {socket.connected ? "Connected to" : "Disconnected from"}{" "}
+              TimeCounter services
+            </motion.h6>
+          </motion.div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
